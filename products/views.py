67a8480246e89521db3f9f825c66a7dc15245e50
Ingvar <Dimon_chick@mail.ru>
2021-08-23 19:22:18 +0900

    added json

--- products/views.py
+++ products/views.py
@@ -1,5 +1,8 @@
+import os
+import json
 from django.shortcuts import render
 
+MODULE_DIR = os.path.dirname(__file__)
 # Create your views here.
 
 def index(request):
@@ -7,13 +10,8 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 def products(request):
+    file_path = os.path.join(MODULE_DIR, 'fixtures/products.json')
     context = {'title': 'GeekShop - Каталог',
-               'products': [
-                   {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090},
-                   {'name': 'Синяя куртка The North Face', 'price': 23725},
-                   {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3390},
-                   {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex', 'price': 13590},
-                   {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN', 'price': 2890},
-                        ]
+              'products': json.load(open(file_path, encoding='utf-8')),
                }
     return render(request, 'products/products.html', context)
