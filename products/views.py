85833ff6d759dc29524061197da9b2f3a1d213f4
Ingvar <Dimon_chick@mail.ru>
2021-08-23 18:25:03 +0900

    added product in views.py & del in products

--- products/views.py
+++ products/views.py
@@ -7,5 +7,13 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 def products(request):
-    context = {'title': 'GeekShop - Каталог'}
+    context = {'title': 'GeekShop - Каталог',
+               'products': [
+                   {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090},
+                   {'name': 'Синяя куртка The North Face', 'price': 23725},
+                   {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3390},
+                   {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex', 'price': 13590},
+                   {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN', 'price': 2890},
+                        ]
+               }
     return render(request, 'products/products.html', context)
