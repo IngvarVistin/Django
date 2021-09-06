15c95bf1f47b04eba7ffdfabe5486d22efad9196
Ingvar <Dimon_chick@mail.ru>
2021-09-07 00:24:58 +0900

    correction location & creaed controller for update users

--- admins/templates/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    context = {'title': 'Geekshop - Admin'}
-    return render(request, 'admins/index.html', context)
-
-def admin_users(request):
-    context = {'title': 'GeekShop - Пользователи', 'users': User.objects.all()}
-    return render(request, 'admins/admin-users.html', context)
---
+++ admins/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+
+from user.models import User
+from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
+
+
+def index(request):
+    context = {'title': 'Geekshop - Admin'}
+    return render(request, 'admins/index.html', context)
+
+
+def admin_users(request):
+    context = {'title': 'GeekShop - Пользователи', 'users': User.objects.all()}
+    return render(request, 'admins/admin-users.html', context)
+
+
+def admin_users_create(request):
+    if request.method == 'POST':
+        form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+    else:
+        form = UserRegistrationForm()
+    context = {'title': 'GeekShop - Creating a user', 'form': form}
+    return render(request, 'admins/admins-users-create.html', context)
+
+
+def admin_users_update(request, id):
+    selected_user = User.objects.get(id=id)
+    if request.method == 'POST':
+        form = UserAdminProfileForm(instance=selected_user, files=request.FILES, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+    else:
+        form = UserAdminProfileForm(instance=selected_user)
+    context = {
+        'title': 'GeekShop - Editing a user',
+        'selected_user': selected_user,
+        'form': form,
+    }
+    return render(request, 'admins/admin-users-update-delete.html', context)
