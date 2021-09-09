cd2b325ac88e34c538af006f4643bc36b283ab65
Ingvar <Dimon_chick@mail.ru>
2021-09-09 22:28:24 +0900

    added UpdateView

--- admins/views.py
+++ admins/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decoration import user_passes_test
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 
 from user.models import User
 from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
@@ -24,22 +24,11 @@ class UserCreateView(CreateView):
     success_url = reverse_lazy('admins:admin_users')
 
 
-@user_passes_test(lambda u: u.is_staff)
-def admin_users_update(request, id):
-    selected_user = User.objects.get(id=id)
-    if request.method == 'POST':
-        form = UserAdminProfileForm(instance=selected_user, files=request.FILES, data=request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('admins:admin_users'))
-    else:
-        form = UserAdminProfileForm(instance=selected_user)
-    context = {
-        'title': 'GeekShop - Editing a user',
-        'selected_user': selected_user,
-        'form': form,
-    }
-    return render(request, 'admins/admin-users-update-delete.html', context)
+class UserUpdateView(UpdateView):
+    model = User
+    tamplate_name = 'admins/users-update-delete.html'
+    form_class = UserAdminProfileForm
+    success_url = reverse_lazy('admins:admin_users')
 
 
 @user_passes_test(lambda u: u.is_staff)
